d53e6640dcad641fe5e702aa791b20ec5fd2131d
Night7407 <164714112+Night7407@users.noreply.github.com>
2026-03-20 08:47:54 +0530

    Commit

--- RedBus/src/main/java/BookingSystem/RedBus/Repo/BusRepository.java
+++ RedBus/src/main/java/BookingSystem/RedBus/Repo/BusRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BusRepository extends JpaRepository<Bus,Long> {
+    List<Bus> findBySourceAndDestination(String source, String destination);
 
 }
